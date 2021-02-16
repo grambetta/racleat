badf5b72be742d0fc1bf0a9e826cde4e1b03baad
Pierre-Emmanuel Daigre <pe.daigre@gmail.com>
2021-02-16 12:56:33 +0100

    Device policies and controllers update

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -8,21 +8,41 @@ class DevicesController < ApplicationController
   end
 
   def new
-    @device = Device.new
+    @device = current_user.devices.new
+    authorize @device
   end
 
   def create
-    @device = Device.new(device_params)
+    @restaurant = current_user.restaurants.new(restaurant_params)
+    authorize @device
     if @device.save
-      redirect_to_device_path
+      redirect_to device_path(@device)
     else
       render :new
     end
   end
 
+  def edit
+    authorize @device
+    @device = Device.find(params[:id])
+  end
+
+  def update
+    authorize @device
+    @device = Device.find(params[:id])
+    @device.update(device_params)
+    redirect_to device_path(@device)
+  end
+
+  def destroy
+    @device = device.find(params[:id])
+    @device.destroy
+    redirect_to devices_path
+  end
+
   private
 
   def device_params
-    params.require(:device).permit(:name, :device_type, :capacity, :brand, :address, :price)
+    params.require(:device).permit(:user_id, :name, :device_type, :capacity, :brand, :address, :price)
   end
 end
