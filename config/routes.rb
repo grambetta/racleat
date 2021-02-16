d5a7b627c65d59de6bd042bb9b473815919552ca
Pierre-Emmanuel Daigre <pe.daigre@gmail.com>
2021-02-16 10:53:58 +0100

    routes updated and created dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :devices, only: [ :index, :show, :new, :create ] do
-    resources :reservations, only: [ :new, :create ]
+  resources :devices do
+    resources :reservations
   end
+  get 'dashboard', to: 'pages#dashboard'
 end
