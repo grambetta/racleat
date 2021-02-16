e7a1e7db7a4622147d304b5c96219fdd7cb1e3f4
Jonathan Klym <joklym@icloud.com>
2021-02-16 15:11:33 +0100

    add device form

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,4 +1,5 @@
 class Device < ApplicationRecord
+  DEVICE_TYPE = ['Traditionnel', 'Electrique']
   belongs_to :user
   has_many :reservations
   validates :name, :device_type, :capacity, :address, :price, presence: true
